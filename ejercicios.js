835c7fa11976c4a3d46de329bef7512b4489cc60
Geraldo <geraldo7888@gmail.com>
2025-10-04 19:23:07 -0500

    se sube actividad

---
+++ ejercicios.js
@@ -0,0 +1,47 @@
+// 1- El terreno rectangular
+
+// let base = parseInt(prompt("Ingrese base:"));
+// let altura = parseInt(prompt("ingrese altura:"));
+// let area = base * altura;
+// let perimetro = 2*(base + altura);
+
+// alert(`El area es: ${area}
+// El perimetro es: ${perimetro}`)
+
+// --------------------------------------------------------
+
+//  Las tres notas del estudiante 
+
+// let nota1 = parseFloat(prompt("Ingrese nota 1: "));
+// let nota2 = parseFloat(prompt("Ingrese nota 2: "));
+// let nota3 = parseFloat(prompt("Ingrese nota 3: "));
+// let promedio = (nota1 + nota2 + nota3)/3;
+
+// alert(`El promedio es: ${promedio}`)
+
+// --------------------------------------------------------
+
+// 3.	El contador de visitas 
+
+// let visitas = 100;
+// visitas += 25;
+// visitas -= 10;
+// visitas *= 2;
+
+// alert(visitas)
+
+// --------------------------------------------------------
+
+// La operación misteriosa 
+
+let operacion = (20+10)/5+3*2-4*(20 + 10) / 5 + 3 * 2 - 4*(20+10)/5+3*2-4
+// Se resuleven parentesis
+operacion = 30/5 + 3*2 -4*30 /5 + 3*2 -4*30 /5 +3*2 -4
+// Se resuelve la multiplicacion
+operacion = 30/5 + 6  -120 /5 + 6 -120 /5 +6 -4
+// Se resuleve la division
+operacion = 6 + 6 -24 +6 -24 +6 -4
+// Se resulven las sumas y restas
+operacion = 12 -18 -18 -4
+
+alert(operacion)
